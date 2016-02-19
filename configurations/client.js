9922e930aee3bb0ef8b0e954cb5852b6a314c959
killua8q8 <killua8q8@gmail.com>
2016-02-19 04:48:41 -0500

    refined structures

--- configurations/client.js
+++ configurations/client.js
@@ -1,10 +1,37 @@
 'use strict';
 
 module.exports = {
-  paths: {
-    js: 'src/client'
+  src: {
+    path: [
+      'src/client'
+    ],
+    js: [
+      'src/client/**/*.js'
+    ],
+    less: [
+      'src/client/**/*.less'
+    ]
   },
-  js: [
-    'src/client/**/*.js'
-  ]
+  build: {
+    path: [
+      'build'
+    ],
+    js: [
+      'build/javascripts/**/*.js'
+    ],
+    css: [
+      'build/css/**/*.css'
+    ]
+  },
+  dist: {
+    path: [
+      'dist/client'
+    ],
+    js: [
+      'dist/client/javascripts/**/*.min.js'
+    ],
+    css: [
+      'dist/client/css/**/*.min.css'
+    ]
+  }
 };
