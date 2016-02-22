2838d9aac94016590ed9b1025c7deed70096d097
killua8q8 <killua8q8@gmail.com>
2016-02-22 03:28:33 -0500

    Added paths for bower components

---
+++ configurations/bower.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Paths and/or file patterns for all required bowercomponents.
+ * Must list in order.
+ *
+ * @module Konko/Global/Configurations/Bower
+ */
+module.exports = {
+  css: [
+    'bower_components/bootstrap/dist/css/bootstrap.min.css',
+    'bower_components/tether/dist/css/tether.min.css',
+    'bower_components/chartist/dist/chartist.min.css',
+    'bower_components/angular-loading-bar/build/loading-bar.min.css',
+  ],
+};
