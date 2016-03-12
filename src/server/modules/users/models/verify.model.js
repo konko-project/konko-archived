987b07d3d38ee5c2b113f31e86f9a7a23b53d9e7
killua8q8 <killua8q8@gmail.com>
2016-03-12 09:13:00 -0500

    changed token to be generated by default

--- src/server/modules/users/models/verify.model.js
+++ src/server/modules/users/models/verify.model.js
@@ -4,41 +4,21 @@ import mongoose from 'mongoose';
 import uuid from 'node-uuid';
 
 /**
+ * Generate a verification token using uuid v4.
+ *
+ * @returns {String} An uuid v4 token
+ */
+const generateToken = () => uuid.v4();
+
+/**
  * Mongoose schema of verification token.
  *
  */
 const verificationTokenSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User',
           required: '{PATH} is required', },
-  token: { type: String, required: '{PATH} is required' },
-  expiresAt: { type: Date, default: Date.now, expires: 12 * 3600 },
+  token: { type: String, default: generateToken },
+  createdAt: { type: Date, default: Date.now, expires: 12 * 60 * 60 },
 });
 
-/**
- * Generate a verification token using uuid v4.
- *
- * @param {Verify~doneCallback} done - A callback to run.
- * @returns {Verify~doneCallback} Callback
- */
-verificationTokenSchema.methods.generateToken = function (done) {
-  const _this = this;
-  const newToken = _this;
-  newToken.token = uuid.v4();
-  newToken.save(err => {
-    if (err) {
-      return done(err);
-    }
-
-    return done(null, newToken.token);
-  });
-};
-
 mongoose.model('VerificationToken', verificationTokenSchema);
-
-/**
- * Callback that handles the generated token.
- *
- * @callback Verify~doneCallback
- * @param {Null} null - null
- * @param {String} token - A uuid v4 token.
- */
