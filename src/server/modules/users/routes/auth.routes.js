bc0a38023163fbbc8ddf8d55f5f1418276375f83
killua8q8 <killua8q8@gmail.com>
2016-05-18 01:20:16 -0400

    added new routes for log out and reset password

--- src/server/modules/users/routes/auth.routes.js
+++ src/server/modules/users/routes/auth.routes.js
@@ -2,6 +2,7 @@
 
 import jwt from 'express-jwt';
 import auth from '../controllers/auth.controller';
+import utils from '../../../configs/utils';
 
 /**
  * Auth routing.
@@ -19,11 +20,17 @@ export default app => {
   app.route('/api/v1/auth/login')
     .post(auth.login(app));
 
+  app.route('/api/v1/auth/logout')
+    .get(auth.getGuest(app));
+
   app.route('/api/v1/auth/sync')
     .get(JWT_AUTH, auth.sync(app));
 
   app.route('/api/v1/verify/:token')
     .get(auth.verify);
+
+  app.route('/api/v1/auth/reset')
+    .post(utils.throttle, auth.resetPass(app));
 
   app.param('token', auth.findToken);
 };
