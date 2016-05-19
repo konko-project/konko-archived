b026cf31bb8c8b500a1ff93553ae44f6ea3eba4b
killua8q8 <killua8q8@gmail.com>
2016-05-19 06:51:52 -0400

    more stuff on Core controller

--- src/server/modules/core/controllers/core.controller.js
+++ src/server/modules/core/controllers/core.controller.js
@@ -17,32 +17,34 @@ export default class CoreController {
   constructor() {}
 
   /**
-   * Response a basic Core info as JSON
+   * Response a Core info as JSON
    *
    * @param {Object} req - HTTP request.
    * @param {Object} res - HTTP response.
-   * @param {nextCallback} next - A callback to run.
    * @static
    */
-  static get(req, res, next) {
-    Core.findOne()
-      .then(core => res.status(200).json(core))
-      .catch(err => next(err));
+  static get({ core }, res) {
+    res.status(200).json(core);
   }
 
   /**
-   * Response a full Core setting as JSON
+   * Response a basic Core setting as JSON
    *
    * @param {Object} req - HTTP request.
    * @param {Object} res - HTTP response.
    * @param {nextCallback} next - A callback to run.
    * @static
    */
-  static basic(req, res, next) {
+  static getFields(req, res, next) {
     Core.findOne()
-      .select('title')
-      .then(core => res.status(200).json(core))
-      .catch(err => next(err));
+      .select(req._fields ? req._fields : 'basic')
+      .exec()
+      .then(core => {
+        let _core = core.toObject();
+        delete _core.admin;
+        delete _core.mailer;
+        res.status(200).json(_core);
+      }).catch(err => next(err));
   }
 
   /**
@@ -54,8 +56,8 @@ export default class CoreController {
    * @static
    */
   static create(req, res, next) {
-    Core.find().then(cores => {
-      if (cores && cores.length) {
+    Core.find().then(([core, ...rest]) => {
+      if (core && core.global.installed) {
         res.status(400).json({ message: 'Bad Request!' });
       } else {
         req.checkBody('basic.title', 'Title cannot be empty!').notEmpty();
@@ -72,6 +74,25 @@ export default class CoreController {
 
   static update(req, res) {
 
+  }
+
+  /**
+   * Middleware that finds core with given id.
+   *
+   * @param {Object} req - HTTP request.
+   * @param {Object} res - HTTP response.
+   * @param {nextCallback} next - A callback to run.
+   * @param {String} id - Mongo object id.
+   * @static
+   */
+  static findCoreById(req, res, next, id) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: 'Core ID is invalid' });
+    }
+
+    Core.findById(id).exec()
+      .then(core => (req.core = core) ? next() : res.status(404).json({ message: 'Core is not found.' }))
+      .catch(err => next(err));
   }
 
 }
