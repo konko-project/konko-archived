970da9f80fd3ce1ce2b2e7fa8b97b61cf4bd9648
killua8q8 <killua8q8@gmail.com>
2016-06-11 12:38:27 -0400

    added permission check to upload api route

--- src/server/modules/core/routes/upload.routes.js
+++ src/server/modules/core/routes/upload.routes.js
@@ -1,32 +1,9 @@
 'use strict';
 
-import fs from 'fs';
-import path from 'path';
 import jwt from 'express-jwt';
+import Uploader from '../../../configs/uploader';
 import upload from '../controllers/upload.controller';
-
-/**
- * Configs multer storage, create directories if not exist.
- *
- * @param {Object} req - HTTP request.
- * @param {Object} res - HTTP response.
- * @param {multerCallback} cb - A callback for multer.
- */
-const storage = (req, file, cb) => {
-  const STATICS = require(path.join(req.app.pwd, 'configurations', 'statics'));
-  const BASE = STATICS.shared.uploads.users;
-  const USER_DIR = path.join(BASE, req.body.userId);
-  const FIELD_DIR = path.join(USER_DIR, file.fieldname);
-  if (!fs.existsSync(path.join(req.app.pwd, USER_DIR))) {
-    fs.mkdirSync(path.join(req.app.pwd, USER_DIR));
-  }
-
-  if (!fs.existsSync(path.join(req.app.pwd, FIELD_DIR))) {
-    fs.mkdirSync(path.join(req.app.pwd, FIELD_DIR));
-  }
-
-  cb(null, path.join(req.app.pwd, FIELD_DIR));
-};
+import permission from '../../../configs/permission';
 
 /**
  * Upload routing and multer configure.
@@ -37,20 +14,10 @@ const storage = (req, file, cb) => {
  */
 export default app => {
   const JWT_AUTH = jwt({ secret: app.get('secret'), userProperty: 'payload' });
-  const profileAUpload = new app.multer(app, storage, null, { fileSize: 2 * 1024 * 1024 });
-  const profileBUpload = new app.multer(app, storage, null, { fileSize: 5 * 1024 * 1024 });
 
   app.route('/api/v1/upload/avatar')
-    .post(JWT_AUTH, profileAUpload.single('avatar'), upload.profileUpload);
+    .post(JWT_AUTH, permission.get('allowUser'), Uploader.upload(app, 'avatar'), upload.profileUpload);
 
   app.route('/api/v1/upload/banner')
-    .post(JWT_AUTH, profileBUpload.single('banner'), upload.profileUpload);
+    .post(JWT_AUTH, permission.get('allowUser'), Uploader.upload(app, 'banner'), upload.profileUpload);
 };
-
-/**
- * Callback for multer.
- *
- * @callback multerCallback
- * @param {Null} null.
- * @param {String} Path that where the file will store.
- */
